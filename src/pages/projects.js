5fc7cdfd1cc35d535363b00491bbd75fbb34528c
Megan Sullivan <meganesulli@gmail.com>
2019-12-23 13:43:13 -0800

    feat: add Projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const Projects = () =>
+  <Layout>
+    <p>This is where I talk about things I've built.</p>
+  </Layout>
+
+export default Projects;
