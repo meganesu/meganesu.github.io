94a7d3115b8c5ad267fc5e170995eceb7fd973b4
Megan Sullivan <meganesulli@gmail.com>
2019-12-23 13:48:06 -0800

    feat: add footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import styles from './styles.module.css';
+
+const Footer = () => 
+<p className={styles.footer}>
+Made by Megan Sullivan © 2019
+</p>
+
+export default Footer;
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../header';
+import Footer from '../footer';
 import styles from './styles.module.css';
 
 const Layout = ({ children }) => (
@@ -8,6 +9,7 @@ const Layout = ({ children }) => (
     <main>
       { children }
     </main>
+    <Footer />
   </div>
 );
 
