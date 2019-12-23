58b36d4873ec85db3640bc288c602204a89ec18a
Megan Sullivan <meganesulli@gmail.com>
2019-12-23 13:46:43 -0800

    feat: convert page title to a link

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import NavBar from './components/nav-bar/';
 import SocialLinks from './components/social-links/';
 import styles from './styles.module.css';
 
 const Header = () => (
   <header className={styles.header}>
-    <h1 className={styles.title}>Megan Sullivan</h1>
+    <Link to="/" className={styles.title}>
+      <h1>Megan Sullivan</h1>
+    </Link>
     <NavBar />
     <SocialLinks />
   </header>
