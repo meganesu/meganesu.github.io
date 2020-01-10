0ae593811b621c456fa3b52c3a46e97a163c8dc1
Megan Sullivan <meganesulli@gmail.com>
2020-01-09 20:49:10 -0800

    feat: add calculation for current year to copyright note in footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react"
 
-import styles from './styles.module.css';
+import styles from "./styles.module.css"
 
-const Footer = () => 
-<p className={styles.footer}>
-Made by Megan Sullivan © 2019
-</p>
+const Footer = () => (
+  <p className={styles.footer}>
+    {`Made by Megan Sullivan © 2019 - ${new Date().getFullYear()}`}
+  </p>
+)
 
-export default Footer;
+export default Footer
