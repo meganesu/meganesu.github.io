c67092fe3e5d20162f760675ed01619bcd8983ed
Megan Sullivan <meganesulli@gmail.com>
2020-01-30 21:39:44 -0800

    fix(#4): change SkipLink to use gatsby Link component

--- src/components/skip-link/index.js
+++ src/components/skip-link/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import styles from './styles.module.css';
 
 const SkipLink = (props) => (
-  <a className={styles.container} href={props.link}>
+  <Link className={styles.container} to={props.link}>
     {props.children}
-  </a>
+  </Link>
 )
 
 export default SkipLink
