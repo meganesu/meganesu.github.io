297659b7016e714031fd925fcf9f9e09350e842b
Megan Sullivan <meganesulli@gmail.com>
2020-01-30 21:34:01 -0800

    feat(#4): remove unused About and Contact pages; move content to Home page

--- src/components/header/components/nav-bar/index.js
+++ src/components/header/components/nav-bar/index.js
@@ -5,10 +5,8 @@ import styles from './styles.module.css';
 
 const NavBar = () =>
   <ul className={styles.navBar}>
-    <li><Link to="/about">About</Link></li>
     <li><Link to="/projects">Projects</Link></li>
     <li><Link to="/blog">Blog</Link></li>
-    <li><Link to="/contact">Contact</Link></li>
   </ul>
 
 export default NavBar;
