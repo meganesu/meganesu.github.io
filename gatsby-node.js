9de0900ccbcec00ab52650d58a3961a159d0133b
Megan Sullivan <meganesulli@gmail.com>
2020-01-30 18:57:14 -0800

    feat(#4): add slug field to MarkdownRemark nodes

---
+++ gatsby-node.js
@@ -0,0 +1,17 @@
+const path = require("path")
+
+module.exports.onCreateNode = ({node, actions}) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = path.basename(node.fileAbsolutePath, '.md')
+
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug
+    })
+  }
+
+  console.log(node)
+}
