5eeec40f1cb5adeeca0523f6c7fd0971146a8df0
Megan Sullivan <meganesulli@gmail.com>
2020-01-30 19:39:29 -0800

    feat(#4): inject contents of MarkdownRemark post into blog post template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
-const BlogPost = () => (
+export const query = graphql`
+  query (
+    $slug: String!
+  ){
+    markdownRemark (
+      fields: {
+        slug: {
+          eq: $slug
+        }
+      }
+    ){
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+const BlogPost = (props) => (
   <Layout>
-    This is a blog post!
+    <h1>{ props.data.markdownRemark.frontmatter.title }</h1>
+    <p>{ props.data.markdownRemark.frontmatter.date }</p>
+    <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html}} />
   </Layout>
 )
 
