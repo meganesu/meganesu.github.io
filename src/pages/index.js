0f213a8e6b568f7396f72f9472b197eaf052caea
Megan Sullivan <meganesulli@gmail.com>
2020-01-30 21:34:01 -0800

    feat(#4): remove unused About and Contact pages; move content to Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,14 @@ import Layout from '../components/layout/'
 
 const IndexPage = () => (
   <Layout>
-    <p>Hi! My name is Megan, and this is my website.</p>
+    <p>Here's a little about me:</p>
+    <ul>
+      <li>I'm currently a full-time software developer</li>
+      <li>I love dogs more than all other things on the planet</li>
+      <li>My life goal is to someday have my own homestead</li>
+      <li>In my spare time, I volunteer at <a href="https://www.oaklandanimalservices.org/">Oakland Animal Services</a>. Looking to add a furry friend to your family? Come by!</li>
+    </ul>
+    <p>Want to get in touch? Send me a message on LinkedIn!</p>
   </Layout>
 )
 
