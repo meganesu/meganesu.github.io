029d5c820b38740c6eba688270bd8034b6050837
Megan Sullivan <meganesulli@gmail.com>
2020-01-30 21:24:41 -0800

    feat(#4): add SkipLink to main content

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import SkipLink from '../skip-link'
 import Header from '../header';
 import Footer from '../footer';
 import styles from './styles.module.css';
 
 const Layout = ({ children }) => (
   <div className={styles.container}>
+    <SkipLink link="#main-content">Skip to main content</SkipLink>
     <Header />
-    <main className={styles.main}>
+    <main className={styles.main} id="main-content" tabIndex={-1}>
       { children }
     </main>
     <Footer />
