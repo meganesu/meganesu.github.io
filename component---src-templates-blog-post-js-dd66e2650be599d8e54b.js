177350f7015cf00cb4fc381de376185594e82ed5
Travis CI User <travis@example.org>
2020-02-12 07:40:31 +0000

    Updates

--- component---src-templates-blog-post-js-dd66e2650be599d8e54b.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{yZlL:function(e,t,a){"use strict";a.r(t),a.d(t,"query",(function(){return m}));var n=a("q1tI"),r=a.n(n),l=a("I/Ru"),m="2268885839";t.default=function(e){return r.a.createElement(l.a,null,r.a.createElement("h1",null,e.data.markdownRemark.frontmatter.title),r.a.createElement("p",null,e.data.markdownRemark.frontmatter.date),r.a.createElement("div",{dangerouslySetInnerHTML:{__html:e.data.markdownRemark.html}}))}}}]);
-//# sourceMappingURL=component---src-templates-blog-post-js-dd66e2650be599d8e54b.js.map
